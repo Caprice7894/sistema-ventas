6cf83a4ea5c77d178bd5b01b99464d0c134fc3a4
Daniel Canache <danielcanache7894@gmail.com>
2022-08-12 18:05:36 -0400

    Added the main files for the server, and some coding

---
+++ source/__init__.py
@@ -0,0 +1,30 @@
+import os
+import flask from Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'ventas.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    #app.config['SECRET_KEY'] = 'Tf64lfBP0SCRtgUdoRg'
+
+    from api import db
+    db.init_app(app)
+
+    import api.rutas
+
+    app.register_blueprints(auth.bp)
+
+    return app
